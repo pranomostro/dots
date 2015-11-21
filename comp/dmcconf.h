73ccc6afda055b801dfa9257f8198d0a7bb6abd1
scharlatan <a.regenfuss@gmx.de>
2015-11-21 13:30:25 +0100

    Added dmc configuration.

---
+++ comp/dmcconf.h
@@ -0,0 +1,11 @@
+#define VERSION "0.1"
+#define DMCDIR ".dmc"
+#define DIRPERM 0750
+#define EDITOR "vim"
+#define MAXACC 16 /* maximum number of accounts */
+#define FWDMSG "Begin forwarded message:\n"
+#define REPMSG "On {Date: }, {From: } wrote:\n"
+#define FWDSUB "Fwd: "
+#define REPSUB "Re: "
+#define DEBUG 0
+#define LIMIT 50
