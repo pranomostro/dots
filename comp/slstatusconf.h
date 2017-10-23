73c3a43371b92b930e63aa5c246b2a8464f0190f
pranomostro <pranomostro@posteo.net>
2017-10-23 18:04:37 +0200

    Updated slstatus config.

--- comp/slstatusconf.h
+++ comp/slstatusconf.h
@@ -1,46 +1,66 @@
 /* See LICENSE file for copyright and license details. */
 
-/* how often to update the statusbar (min value == 1) */
-#define UPDATE_INTERVAL 1
+#include "slstatus.h"
+
+/* interval between updates (in ms) */
+static const int interval = 1000;
 
 /* text to show if no value can be retrieved */
-#define UNKNOWN_STR     "none"
+static const char unknown_str[] = "none";
 
-/* statusbar
-- battery_perc (battery percentage) [argument: battery name]
-- battery_state (battery charging state) [argument: battery name]
-- cpu_perc (cpu usage in percent) [argument: NULL]
-- datetime (date and time) [argument: format]
-- disk_free (disk usage in percent) [argument: mountpoint]
-- disk_perc (disk usage in percent) [argument: mountpoint]
-- disk_total (disk usage in percent) [argument: mountpoint]
-- disk_used (disk usage in percent) [argument: mountpoint]
-- entropy (available entropy) [argument: NULL]
-- gid (gid of current user) [argument: NULL]
-- hostname [argument: NULL]
-- ip (ip address) [argument: interface]
-- load_avg (load average) [argument: NULL]
-- ram_free (ram usage in percent) [argument: NULL]
-- ram_perc (ram usage in percent) [argument: NULL]
-- ram_total (ram usage in percent) [argument: NULL]
-- ram_used (ram usage in percent) [argument: NULL]
-- run_command (run custom shell command) [argument: command]
-- temp (temperature in degrees) [argument: temperature file]
-- uid (uid of current user) [argument: NULL]
-- uptime (uptime) [argument: NULL]
-- username (username of current user) [argument: NULL]
-- vol_perc (alsa volume and mute status in percent) [argument: soundcard]
-- wifi_perc (wifi signal in percent) [argument: wifi card interface name]
-- wifi_essid (wifi essid) [argument: wifi card interface name] */
+/* maximum output string length */
+#define MAXLEN 2048
+
+/*
+ * function             description                     argument
+ *
+ * battery_perc         battery percentage              battery name
+ * battery_power        battery power usage             battery name
+ * battery_state        battery charging state          battery name
+ * cpu_perc             cpu usage in percent            NULL
+ * cpu_iowait           cpu iowait in percent           NULL
+ * cpu_freq             cpu frequency in MHz            NULL
+ * datetime             date and time                   format string
+ * disk_free            free disk space in GB           mountpoint path
+ * disk_perc            disk usage in percent           mountpoint path
+ * disk_total           total disk space in GB          mountpoint path
+ * disk_used            used disk space in GB           mountpoint path
+ * entropy              available entropy               NULL
+ * gid                  GID of current user             NULL
+ * hostname             hostname                        NULL
+ * ipv4                 IPv4 address                    interface name
+ * ipv6                 IPv6 address                    interface name
+ * kernel_release       `uname -r`                      NULL
+ * keyboard_indicators  caps/num lock indicators        NULL
+ * load_avg             load average                    format string
+ * num_files            number of files in a directory  path
+ * ram_free             free memory in GB               NULL
+ * ram_perc             memory usage in percent         NULL
+ * ram_total            total memory size in GB         NULL
+ * ram_used             used memory in GB               NULL
+ * run_command          custom shell command            command
+ * swap_free            free swap in GB                 NULL
+ * swap_perc            swap usage in percent           NULL
+ * swap_total           total swap size in GB           NULL
+ * swap_used            used swap in GB                 NULL
+ * temp                 temperature in degree celsius   sensor file
+ * uid                  UID of current user             NULL
+ * uptime               system uptime                   NULL
+ * username             username of current user        NULL
+ * vol_perc             OSS/ALSA volume in percent      "/dev/mixer"
+ * wifi_perc            WiFi signal in percent          interface name
+ * wifi_essid           WiFi ESSID                      interface name
+ */
 static const struct arg args[] = {
-	/* function	format        argument */
-	{ battery_perc,	"| batt: %s |",	"BAT0"},
-	{ ram_perc,	" ram: %s |",		NULL},
-	{ cpu_perc,	" cpu: %s |",		NULL},
-	{ wifi_perc,	" wifi: %s |",	"wlp3s0"},
-	{ brightness,	" light: %s |",	"/sys/class/backlight/radeon_bl0/brightness"},
-	{ metrictime,	" metric: %s |",	NULL},
-	{ datetime,	" date: %s |",	"%F"},
-	{ datetime,	" time: %s |",	"%H:%M:%S"},
-	{ run_command,	" play: %s",	"plng 30"}
+	/* function format          argument */
+	{ battery_perc,	"| batt: %s%% |",	"BAT0" },
+	{ ram_perc,	" ram: %s%% |",		NULL },
+	{ cpu_perc,	" cpu: %s%% |",		NULL },
+	{ wifi_perc,	" wifi: %s%% |",	"wlp3s0" },
+	{ light,	" light: %s |",	"/sys/class/backlight/radeon_bl0/brightness" },
+	{ run_command,	" vol: %s |",	"amixer -M get Master | awk 'END { match($4, /[0-9][0-9][0-9]?%/, a); print(a[0]); }'" },
+	{ metrictime,	" metric: %s |",	NULL },
+	{ datetime,	" date: %s |",	"%F" },
+	{ datetime,	" time: %s |",	"%H:%M:%S" },
+	{ run_command,	" play: %s",	"plng 30" },
 };
