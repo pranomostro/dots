7d81604b2beae8cc49791eea8a34408b45a40d6a
pranomostro <pranomostro@posteo.net>
2017-11-09 13:17:16 +0100

    Added configuration for thunderbird.

--- comp/dwmconf.h
+++ comp/dwmconf.h
@@ -27,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Thunderbird", NULL,    NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
